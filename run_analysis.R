8cf95e5fe134df321b829c4deda08d4bb349375d
yoshiyuki1989 <46697234+yoshiyuki1989@users.noreply.github.com>
2019-03-01 00:04:03 -0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,29 +1,30 @@
-library(dplyr)
-library(plyr)
+library(dplyr) # in order to use %>%
 
 # download zip file
 Url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(Url, destfile = "./data/Dataset.zip", method = "curl")
 
-# open zip file
+# unzip
 unzip("./data/Dataset.zip", exdir ="./data")
 
-# read features(columns' names)
-features <- read.table("./data/UCI HAR Dataset/features.txt")
+# read features and convert easy character
+features <- read.table("./data/UCI HAR Dataset/features.txt")[,2]
+features <- gsub("^t", "time", features)  # t => time
+features <- gsub("^f", "frequency", features)  # f => frequency
 
 # read activity labels
-activity_labels <- read.table("./data/UCI HAR Dataset/activity_labels.txt", col.names = c("activity_ID", "activity_name"))
+activity_labels <- read.table("./data/UCI HAR Dataset/activity_labels.txt", col.names = c("activityID", "activity_name"))
 
 # read train data set
 X_train <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
-colnames(X_train) <- features[,2]
-y_train <- read.table("./data/UCI HAR Dataset/train/y_train.txt", col.names = "activity_ID")
+colnames(X_train) <- features
+y_train <- read.table("./data/UCI HAR Dataset/train/y_train.txt", col.names = "activityID")
 subject_train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
 
 # read test data set
 X_test <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
-colnames(X_test) <- features[,2]
-y_test <- read.table("./data/UCI HAR Dataset/test/y_test.txt", col.names = "activity_ID")
+colnames(X_test) <- features
+y_test <- read.table("./data/UCI HAR Dataset/test/y_test.txt", col.names = "activityID")
 subject_test <- read.table("./data/UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
 
 # Merges the training and the test sets to create one data set.
@@ -32,13 +33,13 @@ merged_test <- cbind(y_test, subject_test, X_test)
 merged_all <- rbind(merged_train, merged_test)
 
 # Extracts only the measurements on the mean and standard deviation for each measurement.
-mean_or_std_col <- grep("mean|std", features[,2], value=TRUE)
-extracted <- merged_all[, c("activity_ID", "subject", mean_or_std_col)]
+mean_or_std_col <- grep("mean|std", features, value=TRUE)
+extracted <- merged_all[, c("activityID", "subject", mean_or_std_col)]
 
 # Uses descriptive activity names to name the activities in the data set.
-extracted_with_names <- merge(extracted, activity_labels, by="activity_ID", all.x=TRUE)
+extracted_with_names <- merge(extracted, activity_labels, all.x=TRUE)
 
 # creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-aggregate(.~ subject + activity_ID, data=extracted_with_names,FUN=mean) %>%
-  arrange(subject, activity_ID) %>%
+  aggregate(.~activityID+subject, data=extracted_with_names[, -82], FUN=mean) %>% #if include activity_name[column No. 82], the activity_name becomes number(not character).
+  merge(activity_labels, all.x=TRUE) %>% # reassign activity_name
   write.table(file = "tidy_data_set.txt", row.names = FALSE)
